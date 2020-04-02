2e900dcce84a1e40368d90e6c40dcf95ddf9fb2a
GNeto <glaudistonson@gmail.com>
2020-04-02 17:50:43 -0300

    backend finalized

---
+++ backend/config/mongodb.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/knowledge_stats', { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected!'))
+  .catch(e => {
+    const msg = 'ERROR: Unable to connect with MongoDB!'
+    console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
+  })
