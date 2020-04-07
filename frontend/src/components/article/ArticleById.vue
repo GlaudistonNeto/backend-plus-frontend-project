bc0ce827a895800221c28aeb2a6da661e1f82b22
GNeto <glaudistonson@gmail.com>
2020-04-07 00:56:48 -0300

    fixing and also starting the authentication process

--- frontend/src/components/article/ArticleById.vue
+++ frontend/src/components/article/ArticleById.vue
@@ -1,52 +1,57 @@
 <template>
-  <div class="article-by-id">
-    <PageTitle icon="fa fa-file-o" :main="article.name" :sub="article.description" />
-    <div class="article-content" v-html="article.content">
-
+    <div class="article-by-id">
+        <PageTitle icon="fa fa-file-o" :main="article.name" :sub="article.description" />
+        <div class="article-content" v-html="article.content"></div>
     </div>
-  </div>
 </template>
 
 <script>
-import { baseApiUrl } from '@/global';
-import axios from 'axios';
-
-import PageTitle from '../template/PageTitle';
+import 'highlightjs/styles/dracula.css'
+import hljs from 'highlightjs/highlight.pack.js'
+import { baseApiUrl } from '@/global'
+import axios from 'axios'
+import PageTitle from '../template/PageTitle'
 
 export default {
-  name: 'ArticleById',
-  components: { PageTitle },
-  data: function() {
-    return {
-      article: {}
+    name: 'ArticleById',
+    components: { PageTitle },
+    data: function() {
+        return {
+            article: {}
+        }
+    },
+    mounted() {
+        const url = `${baseApiUrl}/articles/${this.$route.params.id}`
+        axios.get(url).then(res => this.article = res.data)
+    },
+    updated() {
+        document.querySelectorAll('.article-content pre.ql-syntax').forEach(e => {
+            hljs.highlightBlock(e)
+        })
     }
-  },
-  mouted() {
-    const url = `${baseApiUrl}/articles/${this.$rout.params.id}`
-    axios.get(url).then(res => this.article = res.data)
-  }
 }
 </script>
 
 <style>
-  .article-content {
-    background-color: azure;
-    border-radius: 8px;
-    padding: 20px;
-  }
+    .article-content {
+        background-color: #FFF;
+        border-radius: 8px;
+        padding: 25px;
+    }
 
-  .article-content pre {
-    padding: 20px;
-    border-radius: 8px;
-    font-size: 1.2rem;
-  }
+    .article-content pre {
+        padding: 20px;
+        border-radius: 8px;
+        font-size: 1.2rem;
+        background-color: #1e1e1e;
+        color: #FFF;
+    }
 
-  
     .article-content img {
-      max-width: 100%;
+        max-width: 100%;
     }
 
     .article-content :last-child {
-      margin-bottom: 0px;
+        margin-bottom: 0px;
     }
 </style>
