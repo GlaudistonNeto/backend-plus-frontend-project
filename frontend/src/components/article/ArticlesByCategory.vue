1f527eff74b7a50f170f427a0aa6b37495f329bb
GNeto <glaudistonson@gmail.com>
2020-04-07 00:18:00 -0300

    articles and menu finished

---
+++ frontend/src/components/article/ArticlesByCategory.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="articles-by-category">
+    <PageTitle icon="fa fa-folder-o"
+      :main="category.name" sub="Category" />
+      <ul>
+        <li v-for="article in articles" :key="article.id">
+          <ArticleItem :article="article" />
+        </li>
+      </ul>
+      <div class="load-more">
+        <button v-if="loadMore" 
+          class="btn btn-lg btn-outline-primary"
+          @click="getArticles">Load More Articles</button>
+      </div>
+  </div>
+</template>
+
+<script>
+import { baseApiUrl } from '@/global';
+import axios from 'axios';
+import PageTitle from '../template/PageTitle';
+import ArticleItem from './ArticleItem'
+
+export default {
+  name: 'ArticlesByCategory',
+  components: { PageTitle, ArticleItem },
+  data: function() {
+    return {
+      category: {},
+      articles: [],
+      page: 1,
+      loadMore: true
+    }
+  },
+  methods: {
+    getCathegory() {
+      const url = `${baseApiUrl}/categories/${this.category.id}`
+      axios(url).then(res => this.category = res.data)
+    }
+  },
+  mounted() {
+    this.category.id = this.$route.params.id
+    this.getCathegory()
+  },
+  getArticles() {
+    const url = `${baseApiUrl}/categories/${this.category.id}/articles?${this.page}`
+      axios(url).then(res => {this.articles = this.articles.concat(res.data)
+      this.page++
+      
+      if(res.data.length === 0) this.loadMore = false
+      })
+  }
+}
+</script>
+
+<style>
+  .articles-by-category ul {
+    list-style-type: none;
+    padding: 0px;
+  }
+
+  .articles-by-category .load-more {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 25px;
+  }
+</style>
--- frontend/src/config/msgs.js
+++ frontend/src/config/msgs.js
@@ -1,19 +1,19 @@
-import Vue from 'vue';
-import Toasted from 'vue-toasted';
+import Vue from 'vue'
+import Toasted from 'vue-toasted'
 
 Vue.use(Toasted, {
-  iconPack: 'fontawesome',
-  duration: 3000
-});
+    iconPack: 'fontawesome',
+    duration: 3000
+})
 
 Vue.toasted.register(
-  'defaultSuccess',
-  payload => !payload.msg ? 'Oops unexpected error.' : payload.msg,
-  { type: 'success', icon: 'check' }
-);
+    'defaultSuccess',
+    payload => !payload.msg ? 'Operation performed successfully!' : payload.msg,
+    { type: 'success', icon: 'check' }
+)
 
 Vue.toasted.register(
-  'defaultError',
-  payload => !payload.msg ? 'Oops unexpected error.' : payload.msg,
-  { type : 'error', icon : 'times' }
-);
+    'defaultError',
+    payload => !payload.msg ? 'Oops.. Unexpected Error.' : payload.msg,
+    { type : 'error', icon : 'times' }
+)
